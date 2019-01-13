faf3c4cdb7e3ae6de2ca4bcfcfd5f4a45b62b58b
shadowdeveloper <ro_web1@hotmail.com>
2019-01-13 07:48:25 -0200

    Layout do card dos politicos e organizaçao de estilo

--- src/components/ListCandidate/CandidateCard/candidateCard.js
+++ src/components/ListCandidate/CandidateCard/candidateCard.js
@@ -10,8 +10,32 @@ class CandidateCard extends Component {
 
   render() {
     return (
-      <section>
-        <span className={styles.selectCandidate}>{this.props.selectCandidateText}</span>
+      <section className={styles.candidateCard}>
+        <div className={`row ${styles.cardPadding}`}>
+          <div className="col-md-3">
+            <div className={styles.candidateImage}></div>
+          </div>
+          <div className="col-md-5">
+            <span className={styles.candidateName}>
+              Nome do cadidato
+            </span>
+            <span className={`d-flex align-items-center ${styles.candidatePoliticalParty}`}>
+              LO <div className={styles.candidateStatusTag}>2° turno</div>
+            </span>
+            <div className={styles.candidateVotesBar}>
+              <div style={{ backgroundColor: "red", width: "50%", height: "100%" }}></div>
+            </div>
+          </div>
+          <div className="col-md-4 text-center">
+            <span className={styles.cadidateVotesPercent}>
+              12,34%
+            </span>
+            <span className={styles.cadidateTotalVotes}>
+              123.345,90<br />votos
+            </span>
+          </div>
+        </div>
+        {/* <span className={styles.selectCandidate}>{this.props.selectCandidateText}</span> */}
       </section>
     )
   }
